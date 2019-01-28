2e30201ac9979da82534eb6955224a6ef843afe1
Matheus Souza <matheu.sk.9@hotmail.com>
2019-01-28 14:24:45 -0200

    Resolve mmissing next call

--- src/state/store.js
+++ src/state/store.js
@@ -15,9 +15,11 @@ const stateData = {
 
 // store
 const logger = store => next => action => {
+    let result
     console.groupCollapsed("dispatching", action.type)
     console.log('prev state', store.getState())
     console.log('action', action)
+    result = next(action)
     console.log('next state', store.getState())
     console.groupEnd()
 }
