7a0eaa3b7a6fd50c3c6da3827542d1fea86d0313
Matheus Souza <matheu.sk.9@hotmail.com>
2019-02-01 18:19:07 -0200

    Finish field prototype

--- src/components/gameScreen/gameScreen.jsx
+++ src/components/gameScreen/gameScreen.jsx
@@ -11,9 +11,67 @@ const Game = ({store}) => {
         )}>
             <tbody>
                 <tr>
-                {squares.map(square =>
-                    <td key={square.id}>{square.symbol}</td>)
-                }
+                {squares.map(square => {
+                    if (square.id < 9) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 8 && square.id < 18) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 17 && square.id < 27) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 26 && square.id < 36) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 35 && square.id < 45) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 44 && square.id < 54) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 53 && square.id < 63) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 62 && square.id < 72) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
+                </tr>
+                <tr>
+                {squares.map(square => {
+                    if (square.id > 71 && square.id < 81) {
+                        return <td key={square.id}>{square.symbol}</td>
+                    } else {return ''}
+                })}
                 </tr>
             </tbody>
         </table> 
