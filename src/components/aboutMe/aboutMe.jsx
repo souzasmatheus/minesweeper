417f82f8d253f2dbc1d37fd1791b900d00cb3ef5
Matheus Souza <matheu.sk.9@hotmail.com>
2019-02-21 18:46:33 -0300

    Add untracked files

---
+++ src/components/aboutMe/aboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './aboutMe.css'
+
+const AboutMe = () => (
+    <div className="about-me">
+        <p>I am Matheus Souza and this is a portfolio project I have developed using Redux. </p>
+        <p>Check out my other projects on Github <a href="https://github.com/souzasmatheus/" rel="noopener noreferrer" target="_blank">
+            <i className="fab fa-github"></i>
+        </a>!</p>
+    </div>
+)
+
+export default AboutMe
---
+++ src/components/boomModal/boomModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './boomModal.css'
+
+const BoomModal = () => {
+    return (
+        <div className="modal">
+            <h1>Booom!!!</h1>
+        </div>
+    )
+}
+
+export default BoomModal
